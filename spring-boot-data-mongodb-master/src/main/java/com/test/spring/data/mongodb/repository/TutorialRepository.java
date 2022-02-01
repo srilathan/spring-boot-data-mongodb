06f2d7b63fde8802dff1382d112a3b8d52df6460
srilathan <srilathan25@gmail.com>
2022-02-01 07:07:31 +0530

    Add files via upload

---
+++ spring-boot-data-mongodb-master/src/main/java/com/test/spring/data/mongodb/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.test.spring.data.mongodb.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.test.spring.data.mongodb.model.Tutorial;
+
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+  List<Tutorial> findByPublished(boolean published);
+  List<Tutorial> findByTitleContaining(String title);
+}
